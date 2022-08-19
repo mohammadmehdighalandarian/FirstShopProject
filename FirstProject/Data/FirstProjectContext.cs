553dc9258ec0f3f18547143214fd4e1a54dc4163
mohammadmehdighn@gmail.com <mohammadmehdighn@gmail.com>
2022-08-19 14:35:26 +0430

    set Seed(difualt value)For Table Categori in SQl

--- FirstProject/Data/FirstProjectContext.cs
+++ FirstProject/Data/FirstProjectContext.cs
@@ -11,5 +11,35 @@ namespace FirstProject.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "تکنولوژی",
+                    Descreption = "تکنولوژی"
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "پوشاک",
+                    Descreption = "پوشاک"
+                }, 
+                new Category
+                {
+                    Id = 3,
+                    Name = "لوازم جانبی خودرو",
+                    Descreption = "لوازم جانبی خودرو"
+                }, new Category
+                {
+                    Id = 4,
+                    Name = "مواد غذایی",
+                    Descreption = "مواد غذایی"
+                }
+            );
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
