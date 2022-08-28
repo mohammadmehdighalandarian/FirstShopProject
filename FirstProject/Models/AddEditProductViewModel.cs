4ad18e473087357d4f4f2424a0c74619289a0c57
mohammadmehdighn@gmail.com <mohammadmehdighn@gmail.com>
2022-08-29 01:54:14 +0430

    make admin panel

---
+++ FirstProject/Models/AddEditProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace FirstProject.Models
+{
+    public class AddEditProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Discreption { get; set; }
+        public int Quintityinstock { get; set; }
+        public decimal Price { get; set; }
+        public IFormFile Picture { get; set; }
+    }
+}
