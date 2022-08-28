a610a0242fe1018efd085acce8c7a5f868023245
mohammadmehdighn@gmail.com <mohammadmehdighn@gmail.com>
2022-08-29 02:08:04 +0430

    Revert "Revert "make admin panel""
    This reverts commit cc00f088b888737911f08af6ce3b56839edab06b.

--- FirstProject/Pages/Admin/Add.cshtml.cs
+++ FirstProject/Pages/Admin/Add.cshtml.cs
@@ -1,3 +1,5 @@
+using FirstProject.Data;
+using FirstProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,59 @@ namespace FirstProject.Pages.Admin
 {
     public class AddModel : PageModel
     {
+        [BindProperty]             //input ha ro vasl mikone be maghadirshon
+        public AddEditProductViewModel Product { get; set; }
+
+        FirstProjectContext _firstProjectContext;
+
+        public AddModel(FirstProjectContext firstProjectContext)
+        {
+            _firstProjectContext = firstProjectContext;
+        }
+
         public void OnGet()
         {
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            var item = new Item()
+            {
+                Price = Product.Price,
+                QuantityInStock = Product.Quintityinstock
+            };
+            _firstProjectContext.Add(item);
+            _firstProjectContext.SaveChanges();
+
+            var pro = new Product()
+            {
+                Name = Product.Name,
+                Description = Product.Discreption,
+                Item = item
+            };
+
+            _firstProjectContext.Add(pro);
+            _firstProjectContext.SaveChanges();
+
+            pro.ItemId = pro.Id;
+            _firstProjectContext.SaveChanges();
+
+            if (Product.Picture?.Length > 0)
+            {
+
+                string filePath = Path.Combine(Directory.GetCurrentDirectory(),
+                    "wwwroot",
+                    "images",
+                    pro.Id + Path.GetExtension(Product.Picture.FileName));
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    Product.Picture.CopyTo(stream);
+                }
+            }
+
+            return RedirectToPage("Index");
         }
     }
 }
